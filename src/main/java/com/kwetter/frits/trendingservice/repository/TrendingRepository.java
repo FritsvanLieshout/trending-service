5921278266890cfcf1c4748a0085a65322017f33
Frits van Lieshout <fritske05@gmail.com>
2021-05-24 15:50:01 +0200

    Added annotation

--- src/main/java/com/kwetter/frits/trendingservice/repository/TrendingRepository.java
+++ src/main/java/com/kwetter/frits/trendingservice/repository/TrendingRepository.java
@@ -2,9 +2,11 @@ package com.kwetter.frits.trendingservice.repository;
 
 import com.kwetter.frits.trendingservice.entity.Trending;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TrendingRepository extends MongoRepository<Trending, String> {
     List<Trending> findTop5ByOrderByCountDesc();
     Trending findTrendingByTrend(String trend);
