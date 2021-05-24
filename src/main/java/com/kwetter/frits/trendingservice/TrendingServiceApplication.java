aee444b33709a60fde0ceb4da7c2399143eed910
Frits van Lieshout <fritske05@gmail.com>
2021-05-24 12:27:49 +0200

    Initial commit

---
+++ src/main/java/com/kwetter/frits/trendingservice/TrendingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kwetter.frits.trendingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendingServiceApplication.class, args);
+	}
+
+}
